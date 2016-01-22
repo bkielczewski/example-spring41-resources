ac28eccf39ab36bccc8678bd1c318918c8d1a632
Thomas Traude <T-Riggs@gmx.net>
2016-01-22 09:18:53 +0100

    Organize imports (remove unused import) in java files

--- src/main/java/eu/kielczewski/example/config/WebMvcConfig.java
+++ src/main/java/eu/kielczewski/example/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package eu.kielczewski.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
